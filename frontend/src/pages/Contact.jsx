9f78c21347ace41b68d70ef11c2a123673f72bd9
sayan chatterjee <chatterjeesayan1009@gmail.com>
2024-07-11 19:19:28 +0530

    login signup page added

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,8 +1,29 @@
+import { useState } from 'react';
+import Login from '../components/Login';
+import Signup from '../components/Signup';
 
-const Contact = () => {
+const ContactPage = () => {
+  const [showLogin, setShowLogin] = useState(true); 
+
+  const toggleForm = () => {
+    setShowLogin(!showLogin); 
+  };
+
   return (
-    <div>Contact</div>
+    <div className="min-h-screen bg-black flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+          {showLogin ? <Login /> : <Signup />}
+          
+          {/* Toggle between login and signup */}
+          <div className="mt-4 text-center">
+            <button
+              onClick={toggleForm}
+              className="text-sm font-medium text-white hover:text-gray-300 focus:outline-none"
+            >
+              {showLogin ? 'Don\'t have an account? Sign up' : 'Already have an account? Log in'}
+            </button>
+          </div>
+        </div>
   )
-}
+};
 
-export default Contact
+export default ContactPage;
